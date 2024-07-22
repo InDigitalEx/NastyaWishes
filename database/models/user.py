2318d6c9e73ceb8976cdcea6e74444576f0eb749
InDigitalEx <InDigitalE8@gmail.com>
2024-07-23 02:37:30 +0400

    v.0.2 (hotfix)

--- database/models/user.py
+++ database/models/user.py
@@ -1,3 +1,4 @@
+from sqlalchemy import BigInteger
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
@@ -7,5 +8,5 @@ class User(Base):
     __tablename__ = 'user'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    telegram_id: Mapped[int] = mapped_column(unique=True)
+    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True)
     is_admin: Mapped[bool] = mapped_column(default=False)
--- database/models/wish.py
+++ database/models/wish.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, ForeignKey, BigInteger
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
@@ -8,7 +8,7 @@ class Wish(Base):
     __tablename__ = 'wish'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    picked_user_id: Mapped[int] = mapped_column(BigInteger, ForeignKey('user.id'), nullable=True)
+    picked_user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=True)
     caption: Mapped[str] = mapped_column(String(1024), nullable=True)
     link_url: Mapped[str] = mapped_column(String(512), nullable=True)
     photo_id: Mapped[str] = mapped_column(String(128), nullable=True)
